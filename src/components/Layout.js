8012cd6fe77c81faadf40ccca575fb1349dc0b9b
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-06-23 00:19:21 +0530

    added components

---
+++ src/components/Layout.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
+import { StaticQuery, graphql } from 'gatsby';
+
+import '../assets/sass/resume.scss';
+
+class Layout extends Component {
+  render() {
+    const { children } = this.props;
+    return (
+      <StaticQuery
+        query={graphql`
+          query SiteTitleQuery {
+            site {
+              siteMetadata {
+                title
+              }
+            }
+          }
+        `}
+        render={data => (
+          <>
+            <Helmet
+              title={data.site.siteMetadata.title}
+              meta={[
+                { name: 'description', content: 'Resume' },
+                { name: 'keywords', content: 'site, web' },
+              ]}
+            >
+              <html lang="en" />
+            </Helmet>
+            <div className={'main-body'}>{children}</div>
+          </>
+        )}
+      />
+    );
+  }
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Layout;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import Scrollspy from 'react-scrollspy';
+import Scroll from './Scroll';
+
+import avatar from '../assets/images/avatar.png';
+import config from '../../config';
+
+export class Sidebar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tabs: [
+        { content: 'About', href: 'about' },
+        { content: 'Experience', href: 'experience' },
+        { content: 'Education', href: 'education' },
+        { content: 'Skills', href: 'skills' },
+        { content: 'Interests', href: 'interests' },
+        { content: 'Awards', href: 'awards' },
+      ],
+    };
+  }
+
+  render() {
+    const { tabs } = this.state;
+    return (
+      <nav
+        className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top"
+        id="sideNav"
+      >
+        <a className="navbar-brand" href="#page-top">
+          <span className="d-block d-lg-none">
+            {config.firstName} {config.lastName}
+          </span>
+          <span className="d-none d-lg-block">
+            <img
+              className="img-fluid img-profile rounded-circle mx-auto mb-2"
+              src={avatar}
+              alt=""
+            />
+          </span>
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <Scrollspy
+            items={tabs.map(s => s.href)}
+            currentClassName="active"
+            offset={-300}
+            className="navbar-nav"
+          >
+            {tabs.map((tab, i) => {
+              const { href, content } = tab;
+              return (
+                <li className="nav-item" key={href}>
+                  <Scroll type="id" element={href}>
+                    <a className="nav-link" href={`#${href}`}>
+                      {content}
+                    </a>
+                  </Scroll>
+                </li>
+              );
+            })}
+          </Scrollspy>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Sidebar;
